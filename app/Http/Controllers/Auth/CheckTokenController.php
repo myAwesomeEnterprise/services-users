abdb183a12710a9c67f10b7fd74ead361a4f88c0
Eduardo Cifuentes <edcilop@gmail.com>
2019-05-13 23:20:57 -0500

    [add] is valid token

---
+++ app/Http/Controllers/Auth/CheckTokenController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CheckTokenController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth.api');
+    }
+
+    public function check()
+    {
+        return response([
+            "message" => "Valid token.",
+        ], 200);
+    }
+}
--- routes/api/user/auth.php
+++ routes/api/user/auth.php
@@ -1,8 +1,11 @@
 <?php
 
-Route::namespace('Auth')->group(function() {
-    Route::post('/register','ApiRegisterController@register');
-    Route::post('/logout', 'LogoutController@logout');
-    Route::post('/password/email', 'ForgotPasswordController@sendResetLinkEmail');
-    Route::post('/password/reset', 'ResetPasswordController@reset');
-});
+Route::namespace('Auth')
+    ->prefix('auth')
+    ->group(function () {
+        Route::post('/register', 'ApiRegisterController@register');
+        Route::post('/logout', 'LogoutController@logout');
+        Route::post('/password/email', 'ForgotPasswordController@sendResetLinkEmail');
+        Route::post('/password/reset', 'ResetPasswordController@reset');
+        Route::post('/is-valid', 'CheckTokenController@check');
+    });
