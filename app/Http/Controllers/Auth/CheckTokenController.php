452a0b3418a6c35b222a40b890e40e65503e40dd
Eduardo Cifuentes <edcilop@gmail.com>
2019-05-13 23:41:51 -0500

    [fix] is valid token

--- app/Http/Controllers/Auth/CheckTokenController.php
+++ app/Http/Controllers/Auth/CheckTokenController.php
@@ -9,7 +9,7 @@ class CheckTokenController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth.api');
+        $this->middleware('auth:api');
     }
 
     public function check()
