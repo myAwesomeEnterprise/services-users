fee1bdf89bd1d156c3fb701859cb90e81c5c6efb
Eduardo Cifuentes <edcilop@gmail.com>
2019-06-23 10:51:33 -0500

    [update] add uuid in passport response

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\PassportServiceProvider::class,
 
         // https://github.com/stechstudio/Laravel-PHP-CS-Fixer
         STS\Fixer\FixerServiceProvider::class,
