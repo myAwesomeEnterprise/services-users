69aa9cfc300e02434cbfff1e95c534eb38ffb0c5
Eduardo Cifuentes <ecifl@naemexico.com>
2019-06-26 17:49:53 -0500

    [update] enable auth routes of laravel

--- routes/web/web.php
+++ routes/web/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
-
 Route::get('/home', 'HomeController@index')->name('home');
 */
+
+Auth::routes(['verify' => true]);
